06b1be60e22550430580d8c175c57175a0d402bb
aaronekdahl <aaronekahl@me.com>
2017-03-02 19:53:34 -0800

    inital-commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
-alert("Let's get to work! ")
-// TODO: Build an array of strings that could be answers to magic 8 ball type questions.
+// Todo: Build an array of strings that could be answers to magic 8 ball type questions.
+var answers = ["Yes", "No", "Maybe", "I Don't Know", "Google It"];
 
+function insertAnswer() {
+  var answer = document.answerForm.answer.value;
+  answers.push(answer);
+  console.log(answers);
+  event.preventDefault();
+}
 
-/* TODO: Write a form handler function that will:
-  1. capture the question asked in the form
-  2. randomly select an element from the array of answers
-  3. console log something conversational to user, like "Oh, you want to know [question]? Well, I think... [answer]"
-*/
+function eightBall() {
+
+  var question =
+  document.eightBallForm.question.value;
+
+  var rando = Math.floor(Math.random() * answers.length);
+
+  var answer = answers[rando];
+
+  var output = "Oh you want to know " + question + "... Well, I think... " + answer;
+
+  console.log(output);
+
+  event.preventDefault();
+}
